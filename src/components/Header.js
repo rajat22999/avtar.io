497035ee9b1e48e3b50c3fcb73ceffaea6af22ef
rajat <rajat22999@gmail.com>
2023-12-22 16:23:29 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div className="header">
-      <p>Avtar.io</p>
+      <p>Avtar.io </p>
       <Image src="/jam_menu.svg" width={40} height={40} alt="menu" />
     </div>
   );
